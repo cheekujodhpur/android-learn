e94bf35a167e398671dd40c837dc02183a4f1081
Kumar Ayush <cheekujodhpur@gmail.com>
2018-02-10 16:16:41 +0530

    adding new activity

--- app/src/main/java/org/wncc_iitb/papita/LoginActivity.java
+++ app/src/main/java/org/wncc_iitb/papita/LoginActivity.java
@@ -1,7 +1,13 @@
 package org.wncc_iitb.papita;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +15,31 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText emailEditText = findViewById(R.id.email_text);
+        final EditText passwordEditText = findViewById(R.id.password_edit_text);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (email.equals("cheeku@wncc-iitb.org") && password.equals("password")) {
+                    // Go to next activity
+                    Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+                    Intent loginToHome = new Intent(LoginActivity.this, HomeActivity.class);
+                    LoginActivity.this.startActivity(loginToHome);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Login failed!", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
     }
 }
